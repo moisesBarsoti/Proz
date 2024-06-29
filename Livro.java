415501df6cd8afc00806ae34e7f23932b73f555d
Moisés Barsoti <146322015+moisesBarsoti@users.noreply.github.com>
2024-06-29 14:28:11 -0300

    Subindo Arquivos

---
+++ Livro.java
@@ -0,0 +1,14 @@
+package proz;
+
+public class Livro {
+    String titulo;
+    String autor;
+    String genero;
+    int paginas;
+    String capa;
+    int anoPublicado;
+    String idioma;
+    String editora;
+    String isbn;
+    float preco;
+}
