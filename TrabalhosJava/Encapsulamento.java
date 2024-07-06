7f2615d267ca713bf3ec7e4ee6ce8a61c69b395a
Moisés Barsoti <146322015+moisesBarsoti@users.noreply.github.com>
2024-07-06 15:36:31 -0300

    Subindo Arquivos

---
+++ TrabalhosJava/Encapsulamento.java
@@ -0,0 +1,13 @@
+package proz.TrabalhosJava;
+
+public class Encapsulamento {
+    // As variáveis estão public - Acessível de qualquer lugar
+    public String name1; 
+    public int idade1;
+    // As variáveis estão private - Acessível apenas dentro da classe.
+    private String name2; 
+    private int idade2;
+    // As variáveis estão protected - Acessível dentro do mesmo pacote e por subclasses.
+    protected String name3; 
+    protected int idade3;
+}
